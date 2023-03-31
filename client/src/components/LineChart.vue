c3448e838169aaa971287002817560e517da063c
Tim von Felten <tim.vonfelten@students.fhnw.ch>
2023-03-31 13:35:52 +0200

    Chart JS und Map Component

--- client/src/components/LineChart.vue
+++ client/src/components/LineChart.vue
@@ -13,13 +13,16 @@ import { Chart as ChartJS, Title, Tooltip, Legend, CategoryScale, LinearScale, P
 ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale)
 
 export default {
+  props: ["data1","data2","dataLabel"],
+  setup(props) {},
   name: 'LineChart',
   components: { Line },
   data() {
+    const dataSetfromProps = [{ data: this.data1 },{ data: this.data2 }];
     return {
       chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
+        labels: this.dataLabel,
+        datasets: dataSetfromProps
       },
       chartOptions: {
         responsive: true
