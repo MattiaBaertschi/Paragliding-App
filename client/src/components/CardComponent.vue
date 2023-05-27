d7e037e3159c7805b0e4ef5a83be619c5a3c6f9d
timvonfelten <114213950+timvonfelten@users.noreply.github.com>
2023-05-27 23:37:35 +0200

    Handle Flight selection

--- client/src/components/CardComponent.vue
+++ client/src/components/CardComponent.vue
@@ -6,7 +6,7 @@
     <div class="p-4">
       <div class="text-sm text-gray-600 mb-1">{{ flight.date }} | 
         <RouterLink :to="`buddy/${ flight.user_id }`">
-          @{{ flight.username }}userhandle
+          @{{ flight.username }}
         </RouterLink>
       </div>
       <div class="text-lg font-semibold">{{flight.flight_name}}</div>
@@ -16,22 +16,40 @@
         <div>{{ flight.comment }}</div>
       </div>
     </div>
-    <div class=" bg-light text-center p-2 mx-4 mb-4 tracking-wider rounded hover:bg-black hover:text-white">
-      <RouterLink :to="`flights/view/${ flight.flight_id }`" class="w-full">
+    <div @click="handleFlightSelection(flight.flight_id, flight.user_id)" class="bg-light text-center p-2 mx-4 mb-4 tracking-wider rounded cursor-pointer hover:bg-black hover:text-white">
+      Flug ansehen
+      <!-- <RouterLink :to="`flights/view/${ flight.flight_id }`" class="w-full">
         <a class="w-full">Flug ansehen</a>
-      </RouterLink>
+      </RouterLink> -->
     </div>
   </div>
 </template>
 
 <script setup>
 import ImageCarousel from "@/components/ImageCarousel.vue";
+import { routerKey } from "vue-router";
+import { useSessionStore } from '@/store/user';
+import { useRouter } from 'vue-router';
+
+const sessionStore = useSessionStore()
+const userId = sessionStore.userId
+const router = useRouter();
 
 const props = defineProps({
   flight: {
     type: Object,
   },
 });
+
+function handleFlightSelection (flight_id, flight_user_id){
+  console.log(userId, "VERSUS", flight_user_id)
+  if (userId == flight_user_id){
+    router.push(`flights/view/${ flight_id }`)
+  }
+  else {
+    router.push(`buddyflight/view/${ flight_id }`)
+  }
+}
 
 </script>
 
